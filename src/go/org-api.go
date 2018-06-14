956ac21f448eb736ec707e92838a4241d45e34e3
Mads Hjorth <madsh@me.com>
2018-06-14 22:16:50 +0200

    reformat

--- src/go/org-api.go
+++ src/go/org-api.go
@@ -12,48 +12,53 @@ import (
 	"bytes"
 	"io"
 	"path"
+	"path/filepath"
+	"os"
 )
 
 // Utility to read from filesystem
-func ReadFromFile(path string)(org map[string] interface{}, err error) {
-	data, err := ioutil.ReadFile("./data/"+path)
-	if err!=nil { return }
+func ReadFromFile(path string) (org map[string]interface{}, err error) {
+	data, err := ioutil.ReadFile("./data/" + path)
+	if err != nil {
+		return
+	}
 	j, err := yaml.YAMLToJSON(data)
-	if err != nil { return }
+	if err != nil {
+		return
+	}
 	err = json.Unmarshal(j, &org)
 	return
 }
 
 // Utility to write to filesystem
-func WriteToFile(org map[string] interface{})(err error) {
-	fpath := "./data/"+org["id"].(string)+".yaml"
+func WriteToFile(org map[string]interface{}) (err error) {
+	fpath := "./data/" + org["id"].(string) + ".yaml"
 	b, err := json.Marshal(org)
 	y, err := yaml.JSONToYAML(b)
 
 	// rotate existing files...?
-	return ioutil.WriteFile(fpath,y, 0644)
+	return ioutil.WriteFile(fpath, y, 0644)
 }
 
 // Utility to index on elastic search
-func IndexOrg(result []byte)(err error) {
+func IndexOrg(result []byte) (err error) {
 	response, err := http.Post("http://localhost:9200/index/org/", "application/json", bytes.NewBuffer(result))
 	_, err = ioutil.ReadAll(response.Body)
 	return err
 	//fmt.Println(string(data))
 }
 
-
 // Display a single document describing an Organisation
 // Served directly from filesystem
 func GetOrganisation(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    fpath := params["id"]+".yaml"
-    println("asking for "+fpath)
-    org, err := ReadFromFile(fpath)
-    if err!=nil {
-    	// should test for missing id 404
+	params := mux.Vars(r)
+	fpath := params["id"] + ".yaml"
+	println("asking for " + fpath)
+	org, err := ReadFromFile(fpath)
+	if err != nil {
+		// should test for missing id 404
 		w.WriteHeader(500)
-		w.Write([]byte ("\n" + err.Error()+"\n"))
+		w.Write([]byte ("\n" + err.Error() + "\n"))
 		return
 	}
 	j, err := json.Marshal(org)
@@ -78,7 +83,9 @@ func GetAuthorityByType(w http.ResponseWriter, poclass string, match bool) {
 	w.Write([]byte("[\n"))
 	var first = true
 	for _, file := range files {
-		if path.Ext(file.Name()) != ".yaml" { continue }
+		if path.Ext(file.Name()) != ".yaml" {
+			continue
+		}
 		org, err := ReadFromFile(file.Name())
 		HandleInternalError(err, w)
 		if match {
@@ -93,7 +100,7 @@ func GetAuthorityByType(w http.ResponseWriter, poclass string, match bool) {
 				w.Write(j)
 			}
 		} else {
-			if org["po-classification"] != poclass && org["isSubOrganizationOf"] == nil{
+			if org["po-classification"] != poclass && org["isSubOrganizationOf"] == nil {
 				if !first {
 					w.Write([]byte("\n,\n"))
 				} else {
@@ -128,28 +135,28 @@ func UpdateOrganisation(w http.ResponseWriter, r *http.Request) {
 	j, err := yaml.YAMLToJSON([]byte(Buf.String()))
 	if err != nil {
 		w.WriteHeader(500)
-		fmt.Fprintf(w,"%s Could not parse data file as YAML\n",http.StatusText(500))
-		fmt.Fprintf(w,"Got this error from the parser \n%s\n",err.Error())
+		fmt.Fprintf(w, "%s Could not parse data file as YAML\n", http.StatusText(500))
+		fmt.Fprintf(w, "Got this error from the parser \n%s\n", err.Error())
 		return
 	}
 	if json.Unmarshal(j, &org) != nil {
 		w.WriteHeader(500)
-		fmt.Fprintf(w,"%s Could not convert YAML to JSON\n",http.StatusText(500))
+		fmt.Fprintf(w, "%s Could not convert YAML to JSON\n", http.StatusText(500))
 		return
 	}
 
-
-
 	w.WriteHeader(200)
 	result, err := json.Marshal(org)
-	fmt.Fprintf(w,"%s\n", result)
+	fmt.Fprintf(w, "%s\n", result)
 	err = WriteToFile(org)
 	err = IndexOrg(result)
 
 }
 
 func HandleInternalError(err error, w http.ResponseWriter) {
-	if err == nil {return}
+	if err == nil {
+		return
+	}
 	println(err)
 	println()
 	w.WriteHeader(500)
@@ -159,13 +166,20 @@ func HandleInternalError(err error, w http.ResponseWriter) {
 
 // Starting up a HTTP server
 func main() {
-    router := mux.NewRouter()
-    router.HandleFunc("/org/{id}", GetOrganisation).Methods("GET")
-		router.HandleFunc("/org/{id}", UpdateOrganisation).Methods("POST")
+
+	// make sure we have a dir to store data
+	newpath := filepath.Join("data")
+	if err := os.MkdirAll(newpath, os.ModePerm); err != nil {
+		panic(err)
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/org/{id}", GetOrganisation).Methods("GET")
+	router.HandleFunc("/org/{id}", UpdateOrganisation).Methods("POST")
 
 	router.HandleFunc("/municipalities", GetAllMunicipalities).Methods("GET")
 	router.HandleFunc("/others", GetAllNonMunicipalities).Methods("GET")
 
-    // CORS to allow serveed from localhost
-    log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(router)))
+	// CORS to allow serveed from localhost
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(router)))
 }
